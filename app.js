1e2ba6b13407f9a67155349282c63a48ca62773f
Cody Partington <codythatsme@gmail.com>
2022-10-13 13:04:52 +1100

    installed dependencies

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const fetch = require("node-fetch");
+import * as cheerio from "cheerio";
+import * as urlParser from "url";
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
