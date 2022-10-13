603c652fdcdc7a6778bd6fad3d64781759c0f1a3
Cody Partington <codythatsme@gmail.com>
2022-10-13 13:21:34 +1100

    made imports requires

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fetch = require("node-fetch");
-import * as cheerio from "cheerio";
-import * as urlParser from "url";
+const cheerio = require("cheerio");
+const urlParser = require("url")
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
