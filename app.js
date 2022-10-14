2fa87beca44e2b7f5d7eb8f5a1c33791c71f568d
Cody Partington <codythatsme@gmail.com>
2022-10-14 21:52:21 +1100

    populated endpoints

--- app.js
+++ app.js
@@ -12,11 +12,118 @@ app.get("/api/v1", async (req, res) => {
     res.send({message: "Hello World!"});
 })
 app.post("/api/v1/getDead", async (req, res) => {
-  res.send({ string: postcodeString.slice(0, postcodeString.length - 1) });
-});
+  const url = req.body.newUrl
+
+    const newUrls = req.body.subdomains
+
+    const getUrl = (link) => {
+        if (link.includes('http')) {
+            return link
+        } else if (link.startsWith('/')) {
+            // console.log(`SHOURTCUT ${link}` )
+            return `${url}/${link}`
+        } else {
+            return `${url}/${link}`
+        }
+    }
+
+    const seenUrls = {}
+    const foundOldLinks = {}
+    let crawlCount = 0
+    let pageFinished
+    const crawl = async (url) => {
+        pageFinished = false
+        console.log(`CRAWLING ${url}`)
+        seenUrls[url] = true
+        crawlCount++
+        const res = await fetch(url)
+        const html = await res.text()
+        const $ = cheerio.load(html)
+        const links = $("a").map((i, link) => link.attribs.href).get()
+
+        const { host } = urlParser.parse(url)
+        let i = 0
+
+        for (const link of links.filter(link => link.includes(host))) {
+            if (!newUrls.some((website) => link.toLowerCase().includes(website.toLowerCase()))) {
+                console.log(`Old Link: ${link} found on URL ${url}`);
+                foundOldLinks[url] = foundOldLinks[url] ? [...foundOldLinks[url], link] : [link]
+            }
+            i++
+            if (i === links.filter(link => link.includes(host)).length) {
+                pageFinished = true
+                console.log(`PAGE FINISHED ${url}`)
+            }
+            if (link.includes(newUrls[0]) && !seenUrls[getUrl(link)]) {
+                await crawl(getUrl(link))
+            }
+        }
+    }
+
+    const crawlAll = async () => {
+         return await crawl(url).then(() => {
+            console.log('CRAWL FINISHED')
+            return Object.entries(foundOldLinks).map(([key,value]) => ({url: key, links: value}))
+        })
+    }
+
+    const result = await crawlAll()
+    console.log(result)
+    res.status(200).json({ foundOldLinks: result })
+})
 app.post("/api/v1/findTarget", async (req, res) => {
-  res.send({ string: postcodeString.slice(0, postcodeString.length - 1) });
-});
+  const url = req.body.searchUrl
+
+    const targetUrls = req.body.targetUrls
+
+const getUrl = (link) => {
+    if (link.includes('http')) {
+        return link
+    } else if (link.startsWith('/')) {
+        // console.log(`SHOURTCUT ${link}` )
+        return `${url}/${link}`
+    } else {
+        return `${url}/${link}`
+    }
+}
+
+const seenUrls = {}
+const foundTargetLinks = {}
+let crawlCount = 0
+// let pageFinished
+const crawl = async (url) => {
+    // pageFinished = false
+    console.log(`Crawling ${url}`)
+    seenUrls[url] = true
+    crawlCount ++
+    const res = await fetch(url)
+    const html = await res.text()
+    const $ = cheerio.load(html)
+    const links = $("a").map((i, link) => link.attribs.href).get()
+    const { host } = urlParser.parse(url) 
+    
+    for (const link of links.filter(link => link.includes(host))) {
+        if (targetUrls.some((website) => link.toLowerCase().includes(website.toLowerCase()))) {
+            console.log(`Found Link: ${link} found on URL ${url}`);
+            foundTargetLinks[url] = foundTargetLinks[url] ? [...foundTargetLinks[url], link] : [link]
+        }
+        if (link.includes(url) && !seenUrls[getUrl(link)]) {
+            await crawl(getUrl(link))
+        }
+    }
+}
+
+    const crawlAll = async () => {
+         return await crawl(url).then(() => {
+            console.log('CRAWL FINISHED')
+            return foundTargetLinks
+        })
+    }
+
+    const result = await crawlAll()
+    console.log(result)
+    res.status(200).json(result)
+})
 app.post("/api/v1/bannedSubdomains", async (req, res) => {
   const url = req.body.newUrl
 
@@ -65,6 +172,6 @@ const crawl = async (url) => {
 
     const result = await crawlAll()
     res.status(200).json(result)
-});
+})
 
 module.exports = app;
